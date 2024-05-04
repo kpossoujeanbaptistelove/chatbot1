f7271ac74bc9107ab36b157228b0713cd2c04d4e
kpossoujeanbaptistelove <122450089+kpossoujeanbaptistelove@users.noreply.github.com>
2024-05-04 16:14:43 +0200

    Update codebot1.py

--- codebot1.py
+++ codebot1.py
@@ -1,7 +1,7 @@
 # Importer les packages
 import streamlit as st
 st.set_page_config(page_title="Mon premier Chatbot",page_icon="robot_face")
-from nltk.chat.util import Chat,reflections
+from nltk.chat.util import *
 
 # Définir le dialogue
 pairs = [
